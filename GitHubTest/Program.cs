7b3b337bcc05eb3c1566c6c33b59f35bbccf229a
Administrator <Administrator@ACADEMY-6211VT0>
2016-11-03 10:53:13 +0100

    Add project files.

---
+++ GitHubTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTest
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Hello World!");
+
+            Calculator();
+            WriteSomething();
+
+        }
+
+        private static void WriteSomething()
+        {
+            
+        }
+
+        private static void Calculator()
+        {
+            Console.WriteLine("Ange en siffra");
+           var input1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ange en siffra att addera med den första siffran du angav");
+           var input2 = Convert.ToInt32(Console.ReadLine());
+
+            var sum = input1 + input2;
+
+            Console.WriteLine("Sweet! Summan av de 2 talen är:");
+            Console.WriteLine(sum);
+
+        }
+    }
+}
